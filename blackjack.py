bbc3f3369e4ff5c43be69f3298bb8f4c29170159
Felipe Chagas <lfntchagas@gmail.com>
2021-03-18 22:30:34 +0000

    test

--- blackjack.py
+++ blackjack.py
@@ -19,7 +19,6 @@ class Card:
         return f'{self.rank} of {self.suit}'
 
 
-
 class Deck:
     
     def __init__(self):
@@ -92,6 +91,8 @@ def hit(deck, hand):
     if hand.value > 21:
         print('You bust!!')
 
+
+
     elif pop_card.rank == 'Ace':
 
         if hand.value + pop_card.value > 21:
@@ -116,7 +117,7 @@ def hit(deck, hand):
 def hit_or_stand(deck, hand):
     global playing
 
-    user_choice = input('Type Hit or Stand\n').lower()
+    user_choice = input('\nType Hit or Stand\n').lower()
 
     if user_choice == 'hit':
         hit(deck, hand)
@@ -129,12 +130,14 @@ def hit_or_stand(deck, hand):
 def show_some(player, dealer):
     print('\nDealer cards are:')
     for card in dealer.cards[1:]:
-        print(card)
+        print(card, card.value)
     
 
     print('\nPlayer cards are:')
     for card in player.cards:
-        print(card)
+        print(card, card.value)
+
+    print('player value', player.value)
   
 
 def show_all(player,dealer):
@@ -154,55 +157,98 @@ def show_all(player,dealer):
 def player_busts(player_hand, player_chips):
 
     print(f'Player hand is over 21. current player hand is {player_hand.value}')
-    player_chips.lose_bet(take_bet(player_chips))
+    player_chips.lose_bet(bet_value)
     print(f'Player has now {player_chips.total} Chips :(')
         
 
 def player_wins(player_hand, player_chips):
     print(f'Player WINS! current player hand is {player_hand.value}')
-    player_chips.win_bet(take_bet(player_chips))
+    player_chips.win_bet(bet_value)
     print(f'Player has now {player_chips.total} Chips!!')
         
 
 def dealer_busts(dealer_hand, player_chips):
-    print(f'Dealer LOST! Dealers hand is {dealer_hand.value}')
-    bet_value = take_bet(player_chips) * 2
-    player_chips.win_bet(bet_value)
+    print(f'Dealer LOST! Dealers hand is {dealer_hand.value}') 
+    player_chips.win_bet(bet_value * 2)
     print(f'Player has now {player_chips.total} Chips!!')
     
+   
+def dealer_wins(dealer_hand, player_chips):
+    print(f'Dealer WINS! Dealers hand is {dealer_hand.value}')
 
-
-    
-def dealer_wins():
-    pass
     
 def push():
     pass
 
 
-red_deck = Deck() # Initialize the deck
-player1_chips = Chips()
-player1 = Hand()
+while True:
+    # Print an opening statement
+    print('-'*10 + "Starting BlackJack" + '-'*10)
 
-
-#player_wins(player1, player1_chips)
-dealer_busts(player1, player1_chips)
-
-
-
-
-dealer = Hand()
-
-hit(red_deck, player1)
-hit(red_deck, dealer)
-
-show_all(player1,dealer)
-
-
-
-# while playing:
     
-#     hit_or_stand(red_deck, player1)
+    # Create & shuffle the deck, deal two cards to each player
+    red_deck = Deck()
+    red_deck.shuffle()
+    player = Hand()
+    dealer = Hand()
+
+    for i in range(2):
+        hit(red_deck,player)
+        hit(red_deck,dealer)
+    
+        
+    # Set up the Player's chips
+    player_chips = Chips()
+    
+    # Prompt the Player for their bet
+    bet_value = take_bet(player_chips)
+
+    # Show cards (but keep one dealer card hidden)
+    show_some(player, dealer)
+
+    
+    while playing:  # recall this variable from our hit_or_stand function
+        
+        # Prompt for Player to Hit or Stand
+        hit_or_stand(red_deck, player)
+           
+        # Show cards (but keep one dealer card hidden)
+        show_some(player, dealer)
+ 
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+        if player.value > 21:
+            player_busts(player, player_chips)
+            break        
+
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+    while dealer.value < 17:
+        hit(red_deck, dealer)
+        print('Dealer hands value is', dealer.value)
+
+        if dealer.value > 21:
+            dealer_busts(dealer, player_chips)
+            break
+
+        elif dealer.value > player.value and dealer.value < 21:
+            dealer_wins(dealer, player_chips)
+            break
+
+        else:
+            print(f'player hand {player.value}, dealer hand {dealer.value}')
+            print('PLAYER WIN')
+    
+    
+        # Show all cards
+    show_all(player,dealer)
+
+        # Run different winning scenarios
+        
+    
+    # Inform Player of their chips total 
+    
+    # Ask to play again
+
 
 
 
--- test.py
+++ test.py
@@ -2,11 +2,16 @@ import time
 abc = True
 cba = True
 
-while abc:
-    print('first loop')
-    time.sleep(2)
+# while abc:
+#     print('first loop')
+#     time.sleep(2)
 
-    while cba:
-        print('Second loop')
-        time.sleep(2)
+#     while cba:
+#         print('Second loop')
+#         time.sleep(2)
         
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+for i,b in enumerate(lista):
+    print(i,b)
